27bdc3af999d3fcf05022e7f79aa91afc896b6bd
Jarred Wininger <jarredwininger@gmail.com>
2020-04-05 12:45:47 -0400

    adding line to test jenkins server

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "this script is from github"
-echo "this is testing github hook for jenkins"
+echo "this is testing github hook for jenkins server"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This was added"
